942b9e4a19a1c012546d2e792f2cc193ccddb3d1
BitBangedff <jonlambusn@gmail.com>
2017-04-19 07:59:18 -0700

    Add development notes to gear position module

--- firmware/obd-gateway/include/gear_position.h
+++ firmware/obd-gateway/include/gear_position.h
@@ -12,6 +12,14 @@
 #include <stdint.h>
 
 
+#define GEAR_POSITION_UNKNOWN (0)
+#define GEAR_POSITION_1 (1)
+#define GEAR_POSITION_2 (2)
+#define GEAR_POSITION_3 (3)
+#define GEAR_POSITION_4 (4)
+#define GEAR_POSITION_5 (5)
+#define GEAR_POSITION_6 (6)
+
 // TODO
 
 
--- firmware/obd-gateway/src/gear_position.c
+++ firmware/obd-gateway/src/gear_position.c
@@ -2,6 +2,12 @@
  * @file gear_position.c
  * @brief TODO.
  *
+ * Table calculators and information:
+ *   - http://www.gearingcommander.com/
+ *   - http://woodsware.aciwebs.com/gears/
+ *   - http://www.datamc.org/2013/06/03/gear-position/
+ *   - http://www.datamc.org/2016/08/07/tire-rolling-radius-correcting-wheel-speed/
+ *
  */
 
 
@@ -16,3 +22,43 @@
 #include "error.h"
 #include "gear_position.h"
 
+
+// inputs(wheel_speed_kph, engine_rpm)
+// outputs(gear_position)
+// constant(gear_ratio[])
+
+// gear ratio = engine rpm / countershaft rpm
+
+// TODO - figure out what to put in the table
+// is it really needed?
+
+// [engine_rpm][wheel_speed] = gear_position
+// [engine_rpm][wheel_speed] = ratio
+
+
+static const uint8_t TABLE[2][3] PROGMEM =
+{
+    {0x00, 0x01, 0x02},
+    {0x03, 0x04, 0x05}
+};
+
+
+uint8_t gp_get(
+        const uint16_t engine_rpm,
+        const uint8_t wheel_speed )
+{
+    uint8_t gear_pos = GEAR_POSITION_UNKNOWN;
+
+    // drive_rpm = wheel_rpm ?
+    // ratio = engine_rpm / wheel_rpm
+
+    // for each gear:
+    //   if ratio[gear] == ratio
+    //     return gear
+
+
+    // byte = pgm_read_byte(&(mydata[i][j]));
+    // word = pgm_read_word(&(mydata[i][j]));
+
+    return gear_pos;
+}
