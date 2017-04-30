7db9c836c5284a696cea4c2b0cea27a1ea2e5a46
BitBangedFF <jonlambusn@gmail.com>
2017-04-30 08:34:27 -0700

    Add buffer CAN recv mechanism

--- firmware/can-sd-logger/src/canbus.c
+++ firmware/can-sd-logger/src/canbus.c
@@ -18,45 +18,91 @@
 #include "canbus.h"
 
 
-static st_cmd_t rx_cmd;
+// 15 MOBs available
+#define RX_MOB_CNT (2)
 
 
-static void clear_rx( void )
+typedef struct
 {
-    rx_cmd.status = STATUS_CLEARED;
-    rx_cmd.cmd = CMD_NONE;
-    rx_cmd.ctrl.rtr = 0;
-    rx_cmd.ctrl.ide = 0;
+    st_cmd_t state;
+    uint8_t buffer[8];
+} can_rx_mob;
+
+
+static uint8_t rx_mob_index;
+static can_rx_mob rx_mob_array[RX_MOB_CNT];
+
+
+static can_rx_mob * get_rx_mob( void )
+{
+    return &rx_mob_array[rx_mob_index];
 }
 
 
-static void cmd_rx(
-        const uint8_t dlc,
-        uint8_t * const data_buffer )
+static void inc_rx_mob( void )
 {
-    rx_cmd.id.std = 0;
-    rx_cmd.dlc = dlc;
-    rx_cmd.pt_data = (uint8_t*) data_buffer;
+    rx_mob_index += 1;
 
-    rx_cmd.cmd = CMD_RX;
+    if(rx_mob_index >= RX_MOB_CNT)
+    {
+        rx_mob_index = 0;
+    }
+}
+
+
+static void clear_rx_mob(
+        can_rx_mob * const mob )
+{
+    mob->state.status = STATUS_CLEARED;
+    mob->state.cmd = CMD_NONE;
+    mob->state.ctrl.rtr = 0;
+    mob->state.ctrl.ide = 0;
+}
+
+
+static void load_rx_mob(
+        can_rx_mob * const mob )
+{
+    mob->state.id.std = 0;
+    mob->state.dlc = sizeof(mob->buffer);
+    mob->state.pt_data = (uint8_t*) mob->buffer;
+    mob->state.cmd = CMD_RX;
 
     // send the data to an available MOB
     uint8_t status = CAN_STATUS_ERROR;
     do
     {
-        status = can_cmd(&rx_cmd);
+        status = can_cmd(&mob->state);
     }
     while(status != CAN_CMD_ACCEPTED);
 }
 
 
+static void init_rx_mobs( void )
+{
+    (void) memset(rx_mob_array, 0, sizeof(rx_mob_array));
+
+    rx_mob_index = 0;
+
+    uint8_t idx;
+    for(idx = 0; idx < RX_MOB_CNT; idx += 1)
+    {
+        clear_rx_mob(&rx_mob_array[idx]);
+    }
+
+    for(idx = 0; idx < RX_MOB_CNT; idx += 1)
+    {
+        load_rx_mob(&rx_mob_array[idx]);
+    }
+}
+
+
 void canbus_init( void )
 {
-    memset(&rx_cmd, 0, sizeof(rx_cmd));
-    clear_rx();
-
     // wait for CAN to initialize or wdt will reset
-    while(can_init(0) == 0) {}
+    while(can_init(0) == 0);
+
+    init_rx_mobs();
 }
 
 
@@ -112,29 +158,38 @@ uint8_t canbus_recv(
 {
     uint8_t ret = ERR_NO_DATA;
 
-    if(rx_cmd.cmd == CMD_NONE)
+    uint8_t idx;
+    for(idx = 0; (idx < RX_MOB_CNT) && (ret == ERR_NO_DATA); idx += 1)
     {
-        cmd_rx(*dlc, data);
-    }
+        can_rx_mob * const rx_mob = get_rx_mob();
 
-    // check for completion
-    uint8_t status = can_get_status(&rx_cmd);
+        if(rx_mob->state.cmd == CMD_NONE)
+        {
+            load_rx_mob(rx_mob);
+        }
 
-    if(status == CAN_STATUS_ERROR)
-    {
-        ret = ERR_CANBUS_RX;
-        rx_cmd.cmd = CMD_ABORT;
-        (void) can_cmd(&rx_cmd);
-        clear_rx();
-    }
-    else if(status == CAN_STATUS_COMPLETED)
-    {
-        *id = (uint16_t) rx_cmd.id.std;
-        *dlc = (uint8_t) rx_cmd.dlc;
+        const uint8_t status = can_get_status(&rx_mob->state);
 
-        clear_rx();
-        cmd_rx(*dlc, data);
-        ret = ERR_OK;
+        if(status == CAN_STATUS_ERROR)
+        {
+            ret = ERR_CANBUS_RX;
+            rx_mob->state.cmd = CMD_ABORT;
+            (void) can_cmd(&rx_mob->state);
+            clear_rx_mob(rx_mob);
+        }
+        else if(status == CAN_STATUS_COMPLETED)
+        {
+            *id = (uint16_t) rx_mob->state.id.std;
+            *dlc = (uint8_t) rx_mob->state.dlc;
+            (void) memcpy(data, rx_mob->buffer, rx_mob->state.dlc);
+
+            clear_rx_mob(rx_mob);
+            load_rx_mob(rx_mob);
+
+            ret = ERR_OK;
+        }
+
+        inc_rx_mob();
     }
 
     return ret;
